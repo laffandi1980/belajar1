2e94a27c65261d21ba5d8e3e45272acb9c85f8bb
laffandiPolinema <laffandi@polinema.ac.id>
2025-08-29 09:47:33 +0700

    tambahan belajar.dart

---
+++ lib/belajar.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "Luqman"; // otomatis String
+  int age = 25; // tipe int
+  double height = 170.5; // tipe double
+  bool isActive = true; // tipe bool
+  print("Nama: $name, Umur: $age, Tinggi: $height, Aktif: $isActive");
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  var name = "Luqman"; // otomatis String
+  int age = 25; // tipe int
+  double height = 170.5; // tipe double
+  bool isActive = true; // tipe bool
+  print("Nama: $name, Umur: $age, Tinggi: $height, Aktif: $isActive");
 }
 
 class MyApp extends StatelessWidget {
